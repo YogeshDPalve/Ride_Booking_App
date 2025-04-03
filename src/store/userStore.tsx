9dfb77430fcb27a9f54802d99826831dcd0b537f
Yogesh Palve <dnyyogesh@gmail.com>
2025-04-03 14:23:54 +0530

    user store created to store the user side information

--- src/store/userStore.tsx
+++ src/store/userStore.tsx
@@ -1,3 +1,7 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { mmkvStorage, storage } from "./storage";
+
 type CustomLocation = {
   lattitude: number;
   longitude: number;
@@ -13,3 +17,24 @@ interface UserStorageProps {
   setLocation: (data: CustomLocation) => void;
   clearData: () => void;
 }
+
+export const useUserStore = create<UserStorageProps>()(
+  persist(
+    (set) => ({
+      user: null,
+      location: null,
+      outOfRange: false,
+      setUser: (data) => set({ user: data }),
+      setLocation: (data) => set({ location: data }),
+      setOutOfRange: (data) => set({ outOfRange: data }),
+      clearData: () => set({ user: null, location: null, outOfRange: false }),
+    }),
+    {
+      name: "user-store",
+      partialize: (state) => ({
+        user: state.user,
+      }),
+      storage: createJSONStorage(() => mmkvStorage),
+    }
+  )
+);
